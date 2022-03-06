52c7733c0d134ef84fcf47b51ac72558103e8f7e
theMealena <themealena.games@gmail.com>
2022-03-06 23:04:10 +0100

    CEV_camera added
    camera added to std lib

---
+++ CEV_camera.c
@@ -0,0 +1,494 @@
+//**********************************************************/
+//** Done by  |      Date     |  version |    comment     **/
+//**------------------------------------------------------**/
+//**   CEV    |  2022/03/06   |   1.0    | rev & test / added to CEV_lib
+//**********************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <SDL.h>
+#include <CEV_mixSystem.h>
+#include <CEV_api.h>
+#include <rwtypes.h>
+#include "camera.h"
+
+//reads camera from virtual file
+static int L_cameraTypeRead_RW(SDL_RWops* src, CEV_Camera *dst);
+
+//write camera into virtual file
+static int L_cameraTypeWrite(CEV_Camera *src, FILE *dst);
+
+//reads camera parameter from virtual file
+static int L_cameraParamTypeRead_RW(SDL_RWops* src, CEV_CameraParam *dst);
+
+//write camera into file
+static int L_cameraParamTypeWrite(CEV_CameraParam *src, FILE* dst);
+
+//makes camera following the followMe point
+static void L_cameraFollow(CEV_CameraParam *in);
+
+//makes camera auto scrolling
+static void L_cameraAutoScroll(CEV_CameraParam *in);
+
+//calculate which side to display open field
+static void L_cameraDirectionCalc(CEV_CameraParam *in);
+
+//calculate where FollowMe point should be
+static int L_cameraFollowPos(CEV_CameraParam *in);
+
+//sets off cam status
+static int L_cameraOffScene(CEV_Camera *src);
+
+//calculates where the camera should be
+static void L_cameraTheoricPos(CEV_CameraParam *in);
+
+
+void CEV_cameraInit(CEV_Camera *in, CEV_FCoord* followPt, SDL_Rect constraint, unsigned int changeTime, CEV_CameraMode mode)
+{//Init new camera
+
+    for(int i=0; i<2; i++)
+    {//setting axis parameters
+
+        in->param[i].dirAutoScroll = 0;
+        in->param[i].autoScrollVel = 0;
+        in->param[i].dirLock = 0;
+        in->param[i].velMax = 0;
+        in->param[i].autoReverse = false;
+        in->param[i].mode = mode? mode : CAMERA_THIRD; //avoiding 0
+        CEV_timerInit(&in->param[i].timer, changeTime);
+    }
+
+    in->param[CEV_X].camDim    = SCREEN_WIDTH;
+    in->param[CEV_X].followThis = &followPt->x;
+    in->param[CEV_X].dirAct     = CAMERA_POSITIVE;
+    in->param[CEV_X].posAct     = &in->scrollActPos.x;
+
+
+    in->param[CEV_Y].camDim    = SCREEN_HEIGHT;
+    in->param[CEV_Y].followThis = &followPt->y;
+    in->param[CEV_Y].dirAct     = CAMERA_POSITIVE;
+    in->param[CEV_Y].posAct     = &in->scrollActPos.y;
+
+    in->constraint = constraint;
+
+    in->scrollActPos = (SDL_Rect){
+                            .x = *in->param[CEV_X].followThis - (in->param[CEV_X].camDim / in->param[CEV_X].mode),
+                            .y = *in->param[CEV_Y].followThis - (in->param[CEV_Y].camDim / in->param[CEV_Y].mode),
+                            .w = in->param[CEV_X].camDim,
+                            .h = in->param[CEV_Y].camDim };
+
+    in->param[CEV_X].posCalc = in->scrollActPos.x;
+    in->param[CEV_Y].posCalc = in->scrollActPos.y;
+
+    CEV_rectConstraint(&in->scrollActPos, in->constraint);
+}
+
+
+CEV_Camera *CEV_cameraLoad(char *fileName)
+{//Loads camera from file
+
+    CEV_Camera* result = NULL;
+    SDL_RWops *rwops = SDL_RWFromFile(fileName, "rb");
+
+    if(IS_NULL(rwops))
+    {
+        fprintf(stderr, "Err at %s / %d : %s.\n", __FUNCTION__, __LINE__, SDL_GetError());
+        return NULL;
+    }
+
+    result = CEV_cameraLoad_RW(rwops, true);
+
+    if(IS_NULL(rwops))
+    {
+        fprintf(stderr, "Err at %s / %d : Read write error occured.\n", __FUNCTION__, __LINE__);
+    }
+
+    return result;
+}
+
+
+int CEV_cameraSave(CEV_Camera *src, const char* fileName)
+{//saves camera into file
+    int funcSts = FUNC_OK;
+
+    FILE *dst = fopen(fileName, "wb");
+
+    if(IS_NULL(dst))
+    {
+        fprintf(stderr, "Err at %s / %d : %s.\n", __FUNCTION__, __LINE__, strerror(errno));
+        return FUNC_ERR;
+    }
+
+    if (L_cameraTypeWrite(src, dst) != FUNC_OK)
+    {
+        fprintf(stderr, "Err at %s / %d : %s.\n", __FUNCTION__, __LINE__, strerror(errno));
+        funcSts = FUNC_ERR;
+    }
+
+    fclose(dst);
+
+    return funcSts;
+}
+
+
+CEV_Camera *CEV_cameraLoad_RW(SDL_RWops *src, bool freeSrc)
+{//Loads camera from RWops
+
+    CEV_Camera *result = calloc(1, sizeof(CEV_Camera));
+
+    if(IS_NULL(result))
+    {
+        fprintf(stderr, "Err at %s / %d : %s.\n", __FUNCTION__, __LINE__, strerror(errno));
+        return NULL;
+    }
+
+    if (L_cameraTypeRead_RW(src, result) != FUNC_OK);
+    {
+        fprintf(stderr, "Err at %s / %d : %s.\n", __FUNCTION__, __LINE__, strerror(errno));
+        goto err;
+    }
+
+    if(freeSrc)
+        SDL_RWclose(src);
+
+    return result;
+
+err:
+    if(freeSrc)
+        SDL_RWclose(src);
+
+    free(result);
+
+    return NULL;
+
+}
+
+
+int CEV_cameraUpdate(CEV_Camera *in)
+{//update camera position & status
+
+    int result = 0;
+
+    for(int i=0; i<2; i++)
+    {
+        //recalculating open field side
+        if(in->param[i].autoReverse)
+            L_cameraDirectionCalc(&in->param[i]);
+
+        if(in->param[i].dirAutoScroll)
+            L_cameraAutoScroll(&in->param[i]);
+        else
+            L_cameraFollow(&in->param[i]);
+
+        in->param[i].followPrev = *in->param[i].followThis;
+    }
+
+    CEV_rectConstraint(&in->scrollActPos, in->constraint);
+
+    return L_cameraOffScene(in);
+}
+
+
+void CEV_cameraReset(CEV_Camera *in)
+{//restores camera on follow point
+
+    for(int i=0; i<2; i++)
+    {
+        L_cameraTheoricPos(&in->param[i]);
+        *in->param[i].posAct = in->param[i].posCalc;
+    }
+
+    CEV_rectConstraint(&in->scrollActPos, in->constraint);
+}
+
+
+void CEV_cameraDimensionSet(CEV_Camera *src, int w, int h)
+{//sets camera width / height
+
+    if(w>0)
+        src->param[CEV_X].camDim = src->scrollActPos.w = w;
+    else
+        src->param[CEV_X].camDim = src->scrollActPos.w = SCREEN_WIDTH;
+
+
+    if(h>0)
+        src->param[CEV_Y].camDim = src->scrollActPos.h = h;
+    else
+        src->param[CEV_Y].camDim = src->scrollActPos.h = SCREEN_HEIGHT;
+
+}
+
+
+void CEV_cameraOpenFieldAuto(CEV_Camera *in, unsigned int time, int axis)
+{//enables automatic open field management
+
+    bool thisAxis = axis;
+
+    in->param[thisAxis].autoReverse = true;
+    in->param[thisAxis].timer.preset = time;
+}
+
+
+void CEV_cameraOpenFieldSet(CEV_Camera *in, int direction)
+{//Sets camera open field direction
+
+    int x = direction & (CAMERA_LEFT | CAMERA_RIGHT),
+        y = (direction & (CAMERA_UP | CAMERA_DOWN))>>2;
+
+    if (x)
+    {
+        in->param[CEV_X].dirAct = direction;
+        in->param[CEV_X].autoReverse = false;
+    }
+
+    if (y)
+    {
+        in->param[CEV_Y].dirAct = direction;
+        in->param[CEV_Y].autoReverse = false;
+    }
+}
+
+
+void CEV_cameraDirectionLock(CEV_Camera *in, int direction)
+{//Locks camera movement to direction
+
+    int x = direction & (CAMERA_LEFT | CAMERA_RIGHT),
+        y = (direction & (CAMERA_UP | CAMERA_DOWN))>>2;
+
+    if (x)
+    {
+        in->param[CEV_X].dirLock = x;
+        in->param[CEV_X].autoReverse = false;
+    }
+    else
+        in->param[CEV_X].dirLock = 0;
+
+    if (y)
+    {
+        in->param[CEV_Y].dirLock = y;
+        in->param[CEV_Y].autoReverse = false;
+    }
+    else
+        in->param[CEV_Y].dirLock = 0;
+
+}
+
+
+void CEV_cameraMaxVelocitySet(CEV_Camera* in, int velMax, int direction)
+{//sets camera maximum velocity
+
+    if(direction & (CAMERA_LEFT | CAMERA_RIGHT))
+        in->param[CEV_X].velMax = abs(velMax);
+
+    if(direction & (CAMERA_UP | CAMERA_DOWN))
+        in->param[CEV_Y].velMax = abs(velMax);
+}
+
+
+void CEV_cameraFollowModeSet(CEV_Camera *in, CEV_CameraMode mode, int axis)
+{//Sets camera following mode : half, third, quarter
+
+    bool thisAxis = axis;
+
+    if((mode>=CAMERA_MED) && (mode <=CAMERA_QUART))
+        in->param[thisAxis].mode = mode;
+}
+
+
+void CEV_cameraScrollSet(CEV_Camera* in, bool autoScroll, int direction, int velocity)
+{//Sets camera auto scroll parameters
+
+    if (direction & (CAMERA_LEFT | CAMERA_RIGHT))
+    {
+        in->param[CEV_X].dirAutoScroll  = autoScroll? direction : 0;
+        in->param[CEV_X].autoScrollVel  = autoScroll? velocity : 0;
+        in->param[CEV_X].autoReverse    = !autoScroll;
+    }
+
+    if (direction & (CAMERA_UP | CAMERA_DOWN))
+    {
+        in->param[CEV_Y].dirAutoScroll  = autoScroll? direction>>2 : 0;
+        in->param[CEV_Y].autoScrollVel  = autoScroll? velocity : 0;
+        in->param[CEV_Y].autoReverse    = !autoScroll;
+    }
+}
+
+
+static int L_cameraTypeRead_RW(SDL_RWops* src, CEV_Camera *dst)
+{//reads camera type from RWops
+
+    int funcSts = FUNC_OK;
+
+    for(int i = 0; i<2; i++)
+        L_cameraParamTypeRead_RW(src, &dst->param[i]);
+
+    return funcSts;
+}
+
+
+static int L_cameraTypeWrite(CEV_Camera *src, FILE *dst)
+{//Writes camera type into file
+
+    for(int i = 0; i<2; i++)
+        L_cameraParamTypeWrite(&src->param[i], dst);
+
+    return readWriteErr;
+}
+
+
+static int L_cameraParamTypeRead_RW(SDL_RWops* src, CEV_CameraParam *dst)
+{//Reads camera parameters from RWops
+
+    int funcSts = FUNC_OK;
+
+    dst->autoReverse        = SDL_ReadU8(src);
+    dst->mode               = (CEV_CameraMode)SDL_ReadU8(src);
+    dst->autoScrollVel      = SDL_ReadLE32(src);
+    dst->dirAct             = (int)SDL_ReadLE32(src);
+    dst->dirLock            = (int)SDL_ReadLE32(src);
+
+    return funcSts;
+}
+
+
+static int L_cameraParamTypeWrite(CEV_CameraParam *src, FILE* dst)
+{//Write camera parameters into file
+
+    readWriteErr = 0;
+
+    write_u8(src->autoReverse, dst);
+    write_u8((uint8_t)src->mode, dst);
+    write_u8((uint8_t)src->autoScrollVel, dst);
+    write_u32le(src->dirAct, dst);
+    write_u32le(src->dirLock, dst);
+
+    return readWriteErr;
+}
+
+
+static void L_cameraFollow(CEV_CameraParam *in)
+{// single axis following calculations
+
+    //calculating camera position from follow position
+    L_cameraTheoricPos(in);
+
+    float pos = *in->posAct;
+
+    if ((!in->dirLock)
+        || ((in->dirLock & CAMERA_POSITIVE) && (in->posCalc < *in->posAct))
+        || ((in->dirLock & CAMERA_NEGATIVE) && (in->posCalc > *in->posAct))
+        )
+    {
+        CEV_reachValue(&pos, in->posCalc, in->velMax);
+        *in->posAct = pos;
+    }
+}
+
+
+static void L_cameraAutoScroll(CEV_CameraParam *in)
+{//makes camera auto scrolling
+
+    int position = L_cameraFollowPos(in);
+
+    switch(in->dirAutoScroll)
+    {
+        case CAMERA_NEGATIVE :
+
+            if((position - in->followPrev) > in->autoScrollVel)
+            {
+                L_cameraFollow(in);
+            }
+            else
+                *in->posAct -= in->autoScrollVel;
+        break;
+
+        case CAMERA_POSITIVE :
+
+            if((in->followPrev - position) > in->autoScrollVel)
+            {
+                L_cameraFollow(in);
+                puts("point ahead\n");
+            }
+            else
+            {
+                *in->posAct += in->autoScrollVel;
+                puts("point behind\n");
+            }
+        break;
+
+        default :
+        break;
+    }
+}
+
+
+static void L_cameraDirectionCalc(CEV_CameraParam *in)
+{//calculate which side to display open field
+
+    if(*in->followThis > in->followPrev)
+        in->dirCalc = CAMERA_POSITIVE;
+
+    else if(*in->followThis < in->followPrev)
+        in->dirCalc = CAMERA_NEGATIVE;
+
+    else
+        in->dirCalc = CAMERA_NONE;
+
+    if(in->autoReverse && !in->dirLock && !in->dirAutoScroll)
+    {
+        /* timer update */
+        in->timer.run = in->dirCalc && (in->dirCalc != in->dirAct);
+
+        /* direction change when timer done */
+        if(CEV_timerTon(&in->timer))
+            in->dirAct = in->dirCalc;
+    }
+    //else
+        //in->dirAct = in->dirCalc;
+}
+
+
+static void L_cameraTheoricPos(CEV_CameraParam *in)
+{//where should the camera be ?
+
+    if(in->dirAct & CAMERA_POSITIVE) //going right / down
+        in->posCalc = *in->followThis - (in->camDim / in->mode);
+    else//CAMERA_NEGATIVE //going left / up
+        in->posCalc = *in->followThis - (in->camDim - (in->camDim / in->mode));
+}
+
+
+static int L_cameraFollowPos(CEV_CameraParam *in)
+{//where should the follow point be ?
+
+    int value = 0;
+
+    if(in->dirAct & CAMERA_POSITIVE) //right / down
+        value = *in->posAct + (in->camDim / in->mode);
+    else//left / up
+        value = *in->posAct + (in->camDim - (in->camDim / in->mode));
+
+    return value;
+}
+
+
+static int L_cameraOffScene(CEV_Camera *in)
+{//follow point is off camera value
+    int result = 0;
+
+    //returns any off-camera position
+    if (*in->param[CEV_X].followThis < in->scrollActPos.x)
+        result |= CAMERA_LEFT;
+    else if (*in->param[CEV_X].followThis >= in->scrollActPos.x + in->scrollActPos.w)
+        result |= CAMERA_RIGHT;
+    if (*in->param[CEV_Y].followThis < in->scrollActPos.y)
+        result |= CAMERA_UP;
+    else if (*in->param[CEV_Y].followThis>= in->scrollActPos.y + in->scrollActPos.h)
+        result |= CAMERA_DOWN;
+
+    return result;
+}
---
+++ CEV_camera.h
@@ -0,0 +1,241 @@
+//**********************************************************/
+//** Done by  |      Date     |  version |    comment     **/
+//**------------------------------------------------------**/
+//**   CEV    |  2022/03/06   |   1.0    | rev & test / added to CEV_lib
+//**********************************************************/
+
+#ifndef CAMERA_H_INCLUDED
+#define CAMERA_H_INCLUDED
+
+
+#include "CEV_timer.h"
+#include "CEV_api.h"
+
+#include <SDL.h>
+
+/*moving direction*/
+#define CAMERA_LEFT     0x1 //0001
+#define CAMERA_RIGHT    0x2 //0010
+#define CAMERA_UP       0x4 //0100
+#define CAMERA_DOWN     0x8 //1000
+#define CAMERA_ALL      0xF
+#define CAMERA_NONE     0x0
+#define CAMERA_NEGATIVE      0x1 //going right, up in world
+#define CAMERA_POSITIVE      0x2 //going left, down in world
+
+/*open field is layed out in front of target point to follow
+- is automatic by default, can be disabled with autoReverse = false.
+open field will be kept as is until call to CEV_cameraOpenFieldSet() is called.
+
+- direction lock is shifted 2 bytes for Y axis
+*/
+
+
+//character positionning half screen, third or quarter
+typedef enum S_CameraMode
+{
+    CAMERA_MED = 2, CAMERA_THIRD = 3, CAMERA_QUART = 4
+}
+CEV_CameraMode;
+
+
+typedef struct S_CameraParam
+{
+    int velMax,         /**< max pxl/frame vel */
+        dirAutoScroll,  /**< autoScroll direction */
+        dirAct,         /**< active direction */
+        dirCalc,        /**< recalculated camera direction */
+        dirLock,        /**< direction lock */
+        camDim,         /**< display (render) dimension */
+        posCalc,        /**< calculated camera position */
+        *posAct,        /**< camera real position  */
+        autoScrollVel; /**< autoscroll pxl/frame vel*/
+
+    bool autoReverse;   /**< enables direction change */
+
+    float *followThis,  /**< follow this position */
+           followPrev;  /**< where was I before ? */
+
+    CEV_CameraMode mode; /**< camera display mode */
+
+    CEV_Timer timer;/**< direction change TON*/
+}
+CEV_CameraParam;
+
+
+typedef struct S_Camera
+{
+    CEV_CameraParam param[2]; /**< parameters x, y */
+
+    SDL_Rect scrollActPos, /**< camera position in world pxl */
+             constraint; /**< world dimension pxl */
+}
+CEV_Camera;
+
+
+/** \brief Inits new camera / sets parameters for camera.
+ *
+ * \param in : CEV_Camera* to init.
+ * \param followPt : CEV_FCoord* to be followed by camera.
+ * \param constraint : SDL_Rect in which camera should be constraint (world).
+ * \param changeTime : unsigned int as delay (ms) for camera to react to direction change.
+ * \param mode : CEV_CameraMode as follow mode.
+ *
+ * \return void
+ * \note Is to use with a declared camera VS camera loaded from file.
+ */
+void CEV_cameraInit(CEV_Camera *in, CEV_FCoord* followPt, SDL_Rect constraint, unsigned int changeTime, CEV_CameraMode mode);
+
+
+/** \brief Laods camera from file.
+ *
+ * \param fileName : char* as path and file name to load.
+ *
+ * \return CEV_Camera* on success, NULL on failure.
+ *
+ */
+CEV_Camera *CEV_cameraLoad(char *fileName);
+
+
+/** \brief Saves camera into file.
+ *
+ * \param src : CEV_Camera* to be saved.
+ * \param fileName : const char* as filename to save as.
+ *
+ * \return int : any of std return value.
+ */
+int CEV_cameraSave(CEV_Camera *src, const char* fileName);
+
+
+/** \brief Loads camera from vrtual file.
+ *
+ * \param src : SDL_RWops* to load from.
+ * \param freeSrc : bool set to true closes virtual file.
+ *
+ * \return CEV_Camera* as loaded from Vfile, NULL on error.
+ *
+ * \note src will be treated as asked wether function is successful or not.
+ */
+CEV_Camera *CEV_cameraLoad_RW(SDL_RWops *src, bool freeSrc);
+
+
+/** \brief Automatic camera update.
+ *
+ * \param in : CEV_Camera* to update.
+ *
+ * \return int : One direction if follow point is off camera, 0 otherwise.
+ *
+ * \note : If follow point was to be off camera at bottom, shall return CAMERA_DOWN
+ */
+int CEV_cameraUpdate(CEV_Camera *in);
+
+
+/** \brief Brings camera back on follow point
+ *
+ * \param in : CEV_Camera* which camera.
+ *
+ * \return void
+ */
+void CEV_cameraReset(CEV_Camera *in);
+
+
+/** \brief Sets camera Dimensions.
+ *
+ * \param src : CEV_Camera* to set dimension for
+ * \param w : int as width dimension (pxl).
+ * \param h : int as height dimension (pxl).
+ *
+ * \return void
+ *
+ * \note setting values of 0 or negative will set demension to be render dimension
+ */
+void CEV_cameraDimensionSet(CEV_Camera *src, int w, int h);
+
+
+/** \brief Enables automatic open field.
+ *
+ * \param in : CEV_Camera* to set.
+ * \param time : unsigned int as ms before open field changes.
+ * \param axis : int which axis to modify CEV_X, CEV_Y.
+ *
+ * \return void
+ *
+ */
+void CEV_cameraOpenFieldAuto(CEV_Camera *in, unsigned int time, int axis);
+
+
+/** \brief Sets/forces camera open field direction.
+ *
+ * \param in : CEV_Camera* to set.
+ * \param direction : int as new direction.
+ *
+ * \return void
+ *
+ * \note Does not override automatic direction switch.
+ */
+void CEV_cameraOpenFieldSet(CEV_Camera *in, int direction);
+
+
+/** \brief Locks camera direction.
+ *
+ * \param in : CEV_Camera* to lock.
+ * \param direction : int as wich direction to lock OR'd together.
+ *
+ * \return void
+ *
+ * \note Can be used to disable backtracking.
+ */
+void CEV_cameraDirectionLock(CEV_Camera *in, int direction);
+
+
+/** \brief Sets camera maximum velocity (pxl/frame)
+ *
+ * \param in : CEV_Camera* to set.
+ * \param velMax : int as maximum velocity that can be reached as pxl/frame.
+ * \param direction : int as wich axis to apply this parameter, Or'd together.
+ *
+ * \return void
+ *
+ * \note any of CAMERA_RIGHT / CAMERA_LEFT sets horizontal maximum velocity.
+ * same for UP/DOWN.
+ */
+void CEV_cameraMaxVelocitySet(CEV_Camera* in, int velMax, int direction);
+
+
+/** \brief Set camera following mode.
+ *
+ * \param in : CEV_Camera* which camera to set.
+ * \param mode : CEV_CameraMode following mode.
+ * \param axis : int which axis to set (CEV_X, CEV_Y).
+ *
+ * \return void
+ */
+void CEV_cameraFollowModeSet(CEV_Camera *in, CEV_CameraMode mode, int axis);
+
+
+/** \brief Activate/deactivate camera auto scrolling
+ *
+ * \param in : CEV_Camera* to parameter.
+ * \param autoScroll : bool true to activate, false to deactivate.
+ * \param direction : int as which direction to auto scroll.
+ * \param velocity : int as auto scroll velocity in pxl/frame.
+ *
+ * \return void
+ *
+ */
+void CEV_cameraScrollSet(CEV_Camera* in, bool autoScroll, int direction, int velocity);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // CAMERA_H_INCLUDED
